85af56013716b23b24a14ae886b8be2d67de8c33
Brendan Tang <b@brendantang.net>
2023-05-15 10:29:21 -0400

    Use my framework

--- main.ts
+++ main.ts
@@ -1,34 +1,14 @@
-import { serve } from "https://deno.land/std@0.160.0/http/mod.ts";
+import {
+  file,
+  files,
+  serve,
+} from "https://raw.githubusercontent.com/brendantang/routing-framework/main/mod.ts";
 
-const page = await Deno.readTextFile(`${Deno.cwd()}/index.html`);
-
-async function handleRequest(request: Request): Promise<Response> {
-  const { pathname } = new URL(request.url);
-
-  // This is how the server works:
-  // 1. A request comes in for a specific asset.
-  // 2. We read the asset from the file system.
-  // 3. We send the asset back to the client.
-
-  // Check if the request is for style.css.
-  if (pathname.startsWith("/glasses.svg")) {
-    // Read the style.css file from the file system.
-    const file = await Deno.readFile("./glasses.svg");
-    // Respond to the request with the glasses.svg file.
-    return new Response(file, {
-      headers: {
-        "content-type": "image/svg+xml",
-      },
-    });
-  }
-  if (pathname.startsWith("/favicon.ico")) {
-    const file = await Deno.readFile("./favicon.ico");
-    return new Response(file, {});
-  }
-
-  return new Response(
-    new TextEncoder().encode(page),
-  );
-}
-
-serve(handleRequest);
+const index = file(`${Deno.cwd()}/public/index.html`);
+serve({
+  "/": index,
+  "/:filename+": files(
+    `${Deno.cwd()}/public/`,
+    "filename",
+  ),
+});
