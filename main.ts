a7746020c5068733ae055c91aa27772c6b0aa0c0
Brendan Tang <b@brendantang.net>
2023-05-15 11:49:23 -0400

    Use new version of framework that works with files

--- main.ts
+++ main.ts
@@ -2,13 +2,12 @@ import {
   file,
   files,
   serve,
-} from "https://raw.githubusercontent.com/brendantang/routing-framework/main/mod.ts";
+} from "https://raw.githubusercontent.com/brendantang/routing-framework/e8ac48593b3bf60a19c68146b6847584ef12a4b4/mod.ts";
 
 const index = file(`${Deno.cwd()}/public/index.html`);
 serve({
   "/": index,
-  "/:filename+": files(
-    `${Deno.cwd()}/public/`,
-    "filename",
+  "/:filepath+": files(
+    `${Deno.cwd()}/public`,
   ),
 });
