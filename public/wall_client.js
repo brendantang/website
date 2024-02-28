00894baea151ad5221c97fe6b5a4373157f9c1d6
Brendan Tang <b@brendantang.net>
2024-02-28 09:25:36 -0500

    some styling

--- public/wall_client.js
+++ public/wall_client.js
@@ -3,14 +3,17 @@ const messages = await response.json()
 const entries = document.getElementById("entries")
 
 console.log(messages)
-messages.forEach(({name, message, time}) => {
+messages.forEach(({name, message, time, id}) => {
   entries.insertAdjacentHTML('afterbegin', 
-  `
-      <div class="mv2 pa2">
-        ${name} said:<br />
-        ${message}
-      </div>
-    `)
+   `<hr />
+    <div class="mb3 pv3 ph3 relative">
+      <b class="flex">${name}</b>
+      <div class="pa2 mt2">${message}</div>
+      <time class="f6 db mt2 o-70">${time}</time>
+      <button id="delete_${id}" class="absolute top-1 right-1">
+        DELETE!!
+      </button>
+    </div>`)
 })
 
 console.log(messages)
